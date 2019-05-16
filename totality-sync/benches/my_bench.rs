d81584cdfac1c7b368004e400ba8c183da681d85
AlterionX <ben.xu.cs@gmail.com>
2019-05-15 19:55:36 -0500

    Buffer creating test

--- totality-sync/benches/my_bench.rs
+++ totality-sync/benches/my_bench.rs
@@ -1,19 +1,20 @@
 #[macro_use]
 extern crate criterion;
 
-use criterion::Criterion;
-use criterion::black_box;
+use criterion::{Criterion, black_box, Fun};
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
-    }
+extern crate totality_sync as sync;
+use sync::triple_buffer as tb;
+
+fn tb_create(n: u64) -> (tb::ReadingView<Vec<u8>>, tb::EditingView<Vec<u8>>) {
+    tb::buffer(vec![0; n as usize])
+}
+fn tb_testing(n: u64) -> () {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    c.bench_function("Creation", |b| b.iter(|| tb_create(black_box(5000))));
+    // c.bench_function("Usage", |b| b.iter(|| fibonacci(black_box(20))));
 }
 
 criterion_group!(benches, criterion_benchmark);
