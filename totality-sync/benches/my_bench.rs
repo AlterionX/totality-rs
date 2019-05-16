9ba643528d988f50b84bc89fda12ecf6b26411ec
AlterionX <ben.xu.cs@gmail.com>
2019-05-15 20:46:53 -0500

    Added heavyweight benches

--- totality-sync/benches/my_bench.rs
+++ totality-sync/benches/my_bench.rs
@@ -9,12 +9,65 @@ use sync::triple_buffer as tb;
 fn tb_create(n: u64) -> (tb::ReadingView<Vec<u8>>, tb::EditingView<Vec<u8>>) {
     tb::buffer(vec![0; n as usize])
 }
-fn tb_testing(n: u64) -> () {
+fn tb_swap_read(rv: &mut Option<tb::ReadingView<Vec<u8>>>) {
+    let rv_int = rv.take().unwrap().read().release();
+    rv.replace(rv_int);
+}
+fn tb_swap_edit(ev: &mut Option<tb::EditingView<Vec<u8>>>) {
+    let ev_int = ev.take().unwrap().edit().release();
+    ev.replace(ev_int);
+}
+fn tb_heavy() {
+    let (rv, ev) = tb_create(1_000_000_000);
+    print!("Editor starting up.");
+    let e_th = std::thread::spawn(move || {
+        print!("Editor starting up.");
+        let mut ev = Some(ev);
+        let mut e = None;
+        let mut scratch = 0;
+        for i in 0..7 {
+            print!("Editor running iteration: {:?}", i);
+            e.replace(ev.take().unwrap().edit());
+            // do some editing
+            let e_int = e.as_ref().unwrap();
+            for (rv, ev) in e_int.r().iter().zip(e_int.w().iter_mut()) {
+                *ev = *rv + 1;
+                scratch = *ev;
+            }
+            ev.replace(e.take().unwrap().release());
+            println!("Editor: {:?}", scratch);
+        }
+    });
+    let r_th = std::thread::spawn(move || {
+        let mut rv = Some(rv);
+        let mut r = None;
+        let mut scratch = 0;
+        loop {
+            r.replace(rv.take().unwrap().read());
+            // do some reading
+            for v in r.as_ref().unwrap().r().iter() {
+                scratch = *v;
+            }
+            rv.replace(r.take().unwrap().release());
+            if scratch == 6 {
+                break
+            }
+            // println!("Reader: {:?}", scratch);
+        }
+    });
+    e_th.join().expect("Failed to join editing thread.");
+    r_th.join().expect("Failed to join reading thread.");
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
+    println!("Benchmark starting up.");
+    tb_heavy();
     c.bench_function("Creation", |b| b.iter(|| tb_create(black_box(5000))));
-    // c.bench_function("Usage", |b| b.iter(|| fibonacci(black_box(20))));
+    let (rv, ev) = tb_create(1_000_000_000);
+    let (mut rv, mut ev) = (Some(rv), Some(ev));
+    c.bench_function("Reading Usage", move |b| b.iter(|| tb_swap_read(&mut rv)));
+    c.bench_function("Editing Usage", move |b| b.iter(|| tb_swap_edit(&mut ev)));
+    c.bench_function("Heavy Usage", |b| b.iter(|| tb_heavy()));
 }
 
 criterion_group!(benches, criterion_benchmark);
