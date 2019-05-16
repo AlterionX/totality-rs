ff48bb32dd227db0fe57b6631b3dddd9d2fe4cf4
AlterionX <ben.xu.cs@gmail.com>
2019-05-15 22:06:08 -0500

    replaced heavy bench with light one

--- totality-sync/benches/my_bench.rs
+++ totality-sync/benches/my_bench.rs
@@ -17,6 +17,40 @@ fn tb_swap_edit(ev: &mut Option<tb::EditingView<Vec<u8>>>) {
     let ev_int = ev.take().unwrap().edit().release();
     ev.replace(ev_int);
 }
+fn tb_light() {
+    let (rv, ev) = tb_create(1_000);
+    let e_th = std::thread::spawn(move || {
+        let mut ev = Some(ev);
+        let mut e = None;
+        for _ in 0..7 {
+            e.replace(ev.take().unwrap().edit());
+            // do some editing
+            let e_int = e.as_ref().unwrap();
+            for (rv, ev) in e_int.r().iter().zip(e_int.w().iter_mut()) {
+                *ev = *rv + 1;
+            }
+            ev.replace(e.take().unwrap().release());
+        }
+    });
+    let r_th = std::thread::spawn(move || {
+        let mut rv = Some(rv);
+        let mut r = None;
+        let mut scratch = 0;
+        loop {
+            r.replace(rv.take().unwrap().read());
+            // do some reading
+            for v in r.as_ref().unwrap().r().iter() {
+                scratch = *v;
+            }
+            rv.replace(r.take().unwrap().release());
+            if scratch == 7 {
+                break
+            }
+        }
+    });
+    e_th.join().expect("Failed to join editing thread.");
+    r_th.join().expect("Failed to join reading thread.");
+}
 fn tb_heavy() {
     let (rv, ev) = tb_create(1_000_000_000);
     let e_th = std::thread::spawn(move || {
@@ -59,7 +93,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     let (mut rv, mut ev) = (Some(rv), Some(ev));
     c.bench_function("Reading Usage", move |b| b.iter(|| tb_swap_read(&mut rv)));
     c.bench_function("Editing Usage", move |b| b.iter(|| tb_swap_edit(&mut ev)));
-    c.bench_function("Heavy Usage", |b| b.iter(|| tb_heavy()));
+    c.bench_function("Light Usage", |b| b.iter(|| tb_light()));
+    // c.bench_function("Heavy Usage", |b| b.iter(|| tb_heavy()));
 }
 
 criterion_group!(benches, criterion_benchmark);
